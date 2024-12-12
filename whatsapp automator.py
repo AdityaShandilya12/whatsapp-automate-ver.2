13645c502a71438594189e96ae7bbfa94dcaf627
Aditya Shandilya <catchadityashandilya@gmail.com>
2024-12-12 19:50:47 +0530

    Add files via upload

---
+++ whatsapp automator.py
@@ -0,0 +1,11 @@
+
+#importing libraries
+import pyautogui
+import time
+
+#while loop
+while True:
+    pyautogui.write('hi this is adityas chat bot')
+    pyautogui.press('enter')
+    time.sleep(1)
+    
